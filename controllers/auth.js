924cc7da6563ed56665302b21528468e7e06c66d
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-09-08 20:10:55 +0300

    registration complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
 router.get("/sign-up", (req, res) => {
-  res.render("auth/sign-up.ejs");
+    res.render("auth/sign-up.ejs");
+});
+
+
+router.post('/sign-up', async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    };
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    };
+
+    const hashPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashPassword;
+
+    const newUser = await User.create(req.body);
+    res.send(newUser);
 });
 
 module.exports = router;
